7c5eb99ded2c86af75b4e613ab3ae812dee9486f
SBSYS <sb.sys@outlook.com>
2022-08-26 10:26:08 -0600

    :recycle: - added language support on sidebar

--- src/admin/core/layouts/DashboardLayout/useDashboardLayout.hook.tsx
+++ src/admin/core/layouts/DashboardLayout/useDashboardLayout.hook.tsx
@@ -4,31 +4,35 @@ import { DashboardLayoutContextProps } from './DashboardLayout.props';
 import { NavGroupProps } from 'admin/core/components';
 /* assets */
 import { MdDashboard, MdStore } from 'react-icons/md';
+import { useTranslation } from 'react-i18next';
 
 export const useDashboardLayout = () => {
+    /* states */
+    const { t } = useTranslation();
+
     /* props */
     const backProps: ButtonProps = {
-        title: 'Close sidebar',
+        title: t('dashboard.actions.back'),
         onClick: () => {},
     };
 
     const groups: NavGroupProps[] = [
         {
-            title: 'Home',
+            title: t('dashboard.navigation.home.name'),
             items: [
                 {
                     icon: <MdDashboard />,
-                    text: 'Dashboard',
+                    text: t('dashboard.navigation.home.items.dashboard'),
                     to: '',
                 },
             ],
         },
         {
-            title: 'Root',
+            title: t('dashboard.navigation.root.name'),
             items: [
                 {
                     icon: <MdStore />,
-                    text: 'Companies',
+                    text: t('dashboard.navigation.root.items.companies'),
                     to: 'companies',
                 },
             ],

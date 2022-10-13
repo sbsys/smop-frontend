cb1f01e852461dbe43f53ee61af035a4a6c8f6ee
SBSYS <sb.sys@outlook.com>
2022-10-13 11:04:08 -0600

    :bug: - updated date comparation

--- src/shared/utils/compare-dates.util.ts
+++ src/shared/utils/compare-dates.util.ts
@@ -1,5 +1,10 @@
-import { isAfter, isBefore, isEqual } from 'date-fns';
+import { addDays, isAfter, isBefore, isEqual, subSeconds } from 'date-fns';
 
 export const isAfterOrEqual = (evaluate: Date, base: Date) => isAfter(evaluate, base) || isEqual(evaluate, base);
 
-export const isBeforeOrEqual = (evaluate: Date, base: Date) => isBefore(evaluate, base) || isEqual(evaluate, base);
+export const isBeforeOrEqual = (evaluate: Date, base: Date) => {
+    let newBase = addDays(base, 1);
+    newBase = subSeconds(newBase, 1);
+
+    return isBefore(evaluate, newBase) || isEqual(evaluate, newBase);
+};

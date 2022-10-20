43f274b022a06dbbd99d1f606f4f3e806a270195
SBSYS <sb.sys@outlook.com>
2022-10-20 16:40:53 -0600

    :bug: - updated organization setting serializer

--- src/admin/tenants/serializers/organization-settings.serializer.ts
+++ src/admin/tenants/serializers/organization-settings.serializer.ts
@@ -1,23 +1,29 @@
-import { OrganizationSettingsDTO } from '../types';
+import { OrganizationFile, OrganizationInternationalization, OrganizationSettingsDTO } from '../types';
 
 export const organizationSettingsSerializer = (data: any): OrganizationSettingsDTO => ({
     organizationId: data.settings.organizationId,
-    ownerReference: data.settings.ownerReference,
-    organizationName: data.settings.organizationName,
+    ownerReference: data.settings.ownerReference !== '-' ? data.settings.ownerReference : '',
+    organizationName: data.settings.organizationName !== '-' ? data.settings.organizationName : '',
     multiLanguage: data.settings.multiLanguage,
     maxLangAllow: data.settings.maxLangAllow,
     decimals: data.settings.decimals,
     readySettings: data.settings.readySettings,
-    internationalization: data.settings.internationalization.map((internationalization: any) => ({
-        id: internationalization.id,
-        abbreviation: internationalization.abbreviation,
-        flagpng: internationalization.flagpng,
-        flagsvg: internationalization.flagsvg,
-        isAvailable: internationalization.isAvailable,
-        preferredLanguage: internationalization.preferredLanguage,
-    })),
-    files: data.settings.files.map((file: any) => ({
-        isCover: file.isCover,
-        url: file.url,
-    })),
+    internationalization: data.settings.internationalization.map(
+        (internationalization: any) =>
+            ({
+                id: internationalization.id,
+                abbreviation: internationalization.abbreviation,
+                flagpng: internationalization.flagpng,
+                flagsvg: internationalization.flagsvg,
+                isAvailable: internationalization.isAvailable,
+                preferredLanguage: internationalization.preferredLanguage,
+            } as OrganizationInternationalization)
+    ),
+    files: data.settings.files.map(
+        (file: any) =>
+            ({
+                isCover: file.isCover,
+                url: file.url,
+            } as OrganizationFile)
+    ),
 });

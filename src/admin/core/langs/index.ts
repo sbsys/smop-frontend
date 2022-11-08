9172cd27a1e0a70c57f8e0136a80f7f1616a15b1
SBSYS <sb.sys@outlook.com>
2022-11-08 09:22:23 -0600

    :globe_with_meridians: - added previous language file

--- src/admin/core/langs/index.ts
+++ src/admin/core/langs/index.ts
@@ -1,7 +1,8 @@
 import { en } from './en.lang';
 import es from './es.lang.json';
+import enPrev from './en.lang.json';
 
 export const langs = {
-    en: { translation: { ...en } },
+    en: { translation: { ...enPrev, ...en } },
     es: { translation: { ...es } },
 };

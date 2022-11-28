6558f38b540119e209fb35b8ba16333ceaaa5959
SBSYS <sb.sys@outlook.com>
2022-11-28 13:48:56 -0600

    :poop: - added prev language definition

--- src/admin/core/langs/index.ts
+++ src/admin/core/langs/index.ts
@@ -1,7 +1,9 @@
 import { en } from './en.lang';
+import enPrev from './en.lang.json'
 import { es } from './es.lang';
+import esPrev from './es.lang.json'
 
 export const langs = {
-    en: { translation: { ...en } },
-    es: { translation: { ...es } },
+    en: { translation: { ...en, ...enPrev } },
+    es: { translation: { ...es, ...esPrev } },
 };

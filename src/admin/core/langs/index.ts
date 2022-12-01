f096b406ff4f7922a9a0b71655e76b4b63c9133f
SBSYS <sb.sys@outlook.com>
2022-12-01 11:22:42 -0600

    :fire: - removed unused language files

--- src/admin/core/langs/index.ts
+++ src/admin/core/langs/index.ts
@@ -1,9 +1,7 @@
 import { en } from './en.lang';
-import enPrev from './en.lang.json'
 import { es } from './es.lang';
-import esPrev from './es.lang.json'
 
 export const langs = {
-    en: { translation: { ...en, ...enPrev } },
-    es: { translation: { ...es, ...esPrev } },
+    en: { translation: { ...en } },
+    es: { translation: { ...es } },
 };
